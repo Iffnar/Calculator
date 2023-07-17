f188f6cd0bfdfc5164baa15a56f352dfa96cd18f
Stephan Kleefisch <iffnar@Xubuntu.myguest.virtualbox.org>
2023-07-17 12:12:53 +0200

    setup html and js file; add basic math operator functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Calculator
 Building a basic Calculator including a GUI
+
+This is paret of the Odin Project. The goal is to build a calculator with a functional GUI and dynamic display. 
+It`s supposed to be able to compute basic math operators, have buttons for each digit and a "clear" button.
---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+let a = 6;
+let b = 3;
+
+
+function add(a,b) {
+    const sum = a + b;
+    return sum;
+}
+
+function subtract(a,b) {
+    const difference = a - b;
+    return difference
+}
+
+function multiply(a,b) {
+    const product = a * b;
+    return product;
+}
+
+function division(a,b) {
+    const quotient = a / b;
+    return quotient
+}
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(division(a,b));
