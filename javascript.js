8b18f5278b8615b13beea2973c5da6ccb2beecbd
Stephan Kleefisch <iffnar@Xubuntu.myguest.virtualbox.org>
2023-07-18 20:49:58 +0200

    delete various functions and rebuild

--- javascript.js
+++ javascript.js
@@ -1,154 +1,163 @@
 
 let a
 let b
-let operator = "+";
-let sum;
-let difference;
-let product;
-let quotient;
-let displayValue = "";
+let operator;
+const num = document.querySelectorAll("#num");
+const display = document.querySelector(".display");
 
-// let displayValue = displayText
-
-
+// function to populate display
 
 
 // digit-button functionality
-const butt1 = document.querySelector("#butt1");
-butt1.addEventListener("click", () => {
-   displayValue += 1;
-   document.querySelector(".displayValue").textContent +=1;
-   console.log(displayValue); 
+
+num.forEach((num) => {
+    num.addEventListener("click", () => display.textContent += num.textContent); 
 });    
-const butt2 = document.querySelector("#butt2");
-butt2.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 2;
-    displayValue += 2;
-});   
-const butt3 = document.querySelector("#butt3");
-butt3.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 3;
-    displayValue += 3;
-});
-const butt4 = document.querySelector("#butt4");
-butt4.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 4;
-    displayValue += 4;
-});
-const butt5 = document.querySelector("#butt5");
-butt5.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 5;
-    displayValue += 5;
-});
-const butt6 = document.querySelector("#butt6");
-butt6.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 6;
-    displayValue += 6;
-});
-const butt7 = document.querySelector("#butt7");
-butt7.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 7;
-    displayValue += 7;
-});
-const butt8 = document.querySelector("#butt8");
-butt8.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 8;
-    displayValue += 8;
-});
-const butt9 = document.querySelector("#butt9");
-butt9.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 9;
-    displayValue += 9;
-});
-const butt0 = document.querySelector("#butt0");
-butt0.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += 0;
-    displayValue += 0;
-});
+
+
+
+// const butt2 = document.querySelector("#butt2");
+// butt2.addEventListener("click", () => {
+//     displayText.textContent += 2;
+//     display += 2;
+// });   
+// const butt3 = document.querySelector("#butt3");
+// butt3.addEventListener("click", () => {
+//     displayText.textContent += 3;
+//     display += 3;
+// });
+// const butt4 = document.querySelector("#butt4");
+// butt4.addEventListener("click", () => {
+//     displayText.textContent += 4;
+//     display += 4;
+// });
+// const butt5 = document.querySelector("#butt5");
+// butt5.addEventListener("click", () => {
+//     displayText.textContent += 5;
+//     display += 5;
+// });
+// const butt6 = document.querySelector("#butt6");
+// butt6.addEventListener("click", () => {
+//     displayText.textContent += 6;
+//     display += 6;
+// });
+// const butt7 = document.querySelector("#butt7");
+// butt7.addEventListener("click", () => {
+//     displayText.textContent += 7;
+//     display += 7;
+// });
+// const butt8 = document.querySelector("#butt8");
+// butt8.addEventListener("click", () => {
+//     displayText.textContent += 8;
+//     display += 8;
+// });
+// const butt9 = document.querySelector("#butt9");
+// butt9.addEventListener("click", () => {
+//     displayText.textContent += 9;
+//     display += 9;
+// });
+// const butt0 = document.querySelector("#butt0");
+// butt0.addEventListener("click", () => {
+//     displayText.textContent += 0;
+//     display += 0;
+// });
 const buttDot = document.querySelector("#buttDot");
 buttDot.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent += ".";
-    displayValue += ".";
+    displayText.textContent.includes(".") ? buttDot.disable = true :
+    displayText.textContent += ".";
+    display += ".";
 });
 
 // operator-button functionality
 const buttAdd = document.querySelector("#buttAdd");
 buttAdd.addEventListener("click", () => {
-    a = parseFloat(displayValue); 
-    displayValue = "";
-    console.log(a);
-    document.querySelector(".displayValue").textContent += "+";
-    operator = "+";
+    if (displayText.textContent.includes("-") || displayText.textContent.includes ("*") || displayText.textContent.includes ("/") || displayText.textContent.includes ("+")) {
+        b = parseFloat(display);
+       (operate(a,operator,b));
+       document.querySelector(".currentOperation").textContent = displayText.textContent;
+       a = sum;
+       displayText.textContent = "+";
+       
+    } else { 
+        a = parseFloat(display);
+        display = "";
+        displayText.textContent += "+";
+        console.log(a);
+        operator = "+";
+    };
 });
 const buttSub = document.querySelector("#buttSub");
 buttSub.addEventListener("click", () => {
-    a = parseFloat(displayValue);
-    displayValue = "";
-    console.log(a);
-    document.querySelector(".displayValue").textContent += "-";
-    operator = "-";
-})
+    if (displayText.textContent.includes("-") || displayText.textContent.includes ("*") || displayText.textContent.includes ("/") || displayText.textContent.includes ("+")) {
+        b = parseFloat(display);
+        (operate(a,operator,b));
+        document.querySelector(".currentOperation").textContent = displayText.textContent;
+        a = difference;
+        displayText.textContent += "-";
+    } else {   
+        a = parseFloat(display); 
+        display = "";
+        console.log(a);
+        displayText.textContent += "-";
+        operator = "-";
+}});
 const buttMult = document.querySelector("#buttMult");
 buttMult.addEventListener("click", () => {
-    a = parseFloat(displayValue); 
-    displayValue = "";
+    a = parseFloat(display); 
+    display = "";
     console.log(a);
-    document.querySelector(".displayValue").textContent += "*";
+    displayText.textContent += "*";
     operator = "*";
 });
 const buttDiv = document.querySelector("#buttDiv");
 buttDiv.addEventListener("click", () => {
-    a = parseFloat(displayValue); 
-    displayValue = "";
+    a = parseFloat(display); 
+    display = "";
     console.log(a);
-    document.querySelector(".displayValue").textContent += "/";
+    displayText.textContent += "/";
     operator = "/";
 });
 const buttClear = document.querySelector("#buttClear");
 buttClear.addEventListener("click", () => {
-    document.querySelector(".displayValue").textContent = "";
+    displayText.textContent = "";
     document.querySelector(".currentOperation").textContent = "";
-    displayValue = "";
+    display = "";
 });    
 const buttEqual = document.querySelector("#buttEqual");
 buttEqual.addEventListener("click", () => {
-    document.querySelector(".currentOperation").textContent = document.querySelector(".displayValue").textContent;
+    document.querySelector(".currentOperation").textContent = displayText.textContent;
     console.log(`a = ${a}`);
-    b = parseFloat(displayValue);
+    b = parseFloat(display);
     console.log(`b = ${b}`);
     operate(a,operator,b);
 });    
-
 
 // for calling math functions with number a, b and the chosen operator
 function operate(a, operator, b) {
     if (operator === "+") {
         add(a,b);
-        document.querySelector(".displayValue").textContent = sum;
-        displayValue = sum;
-        console.log(`${displayValue} = a`);
+        displayText.textContent = sum;
+        display = sum;
+        console.log(`${display} = a`);
     } else if (operator === "-") {
-        console.log("difference")
         subtract(a,b);
-        document.querySelector(".displayValue").textContent = difference;
-        displayValue = difference;
+        displayText.textContent = difference;
+        display = difference;
     } else if (operator ==="*") {
         multiply(a,b);
-        document.querySelector(".displayValue").textContent = product;
-        displayValue = product;
+        displayText.textContent = product;
+        display = product;
     } else if (operator ==="/") {
         division(a,b);
         console.log(`quotient = ${quotient}`);
         if (quotient == "Infinity") {
-            document.querySelector(".displayValue").textContent = "You Dummy!";
-            displayValue = quotient;
+            displayText.textContent = "You Dummy!";
+            display = quotient;
         } else {
-            document.querySelector(".displayValue").textContent = quotient ;    
+            displayText.textContent = quotient ;    
         }   
     } 
 };
-
-
 
 // functions for basic math operators; take number a and number b;
 function add(a,b) {
